0fc6de09af130ff5c05ffde0972f23b13367cedf
shubhamOO73414 <syadavji3414@gmail.com>
2021-05-21 23:09:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/*console.log("shubham console");
+var John=80;
+var Nick=45;
+var lowerLimit=51;
+if(John>=51 && Nick>=51){
+  console.log("John and Nick both passed");
+}
+else if(John>=51 || Nick>=51){
+  if(John>=51){
+  console.log("John passed");}
+  else{
+    console.log("Nick passed");
+  }
+}
+else{
+  console.log("both failed");
+}
+var arr=["shubhm","yadav","priyam",23,10,[1,4,6]];
+console.log(arr);*/
+/*var colors=["red","green","blue"];
+console.log(colors);
+colors[1]="white";
+console.log(colors);
+colors.push("black");
+console.log(colors);
+colors.pop();
+console.log(colors);
+colors.shift();
+console.log(colors);
+colors.unshift("sunny")
+console.log(colors);*/
+
+/*var person=new Object();
+person.firstname="shubham";
+person.lastname="yadav";
+person.son=new Object();
+person.son.name="smith";
+person.son.age="23";
+console.log(person);*/
+/*var person1={
+firstName:"shukla",
+lastName:"shukla",
+per:function(dau){
+  console.log(dau.daughtername+" is daughter of "+dau.fathername);
+}
+};
+person1.per({daughtername: 'saniya', fathername: 'shukkhi'});*/
+var date=new Date();
+console.log(date);
